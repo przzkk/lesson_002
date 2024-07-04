56ed49256613463987aa4501eadc5cc439ecda28
przzkk <przzkk@gmail.com>
2024-07-04 10:24:27 +0500

    solved garden

--- 08_garden.py
+++ 08_garden.py
@@ -12,16 +12,30 @@ meadow = ('клевер', 'одуванчик', 'ромашка', 'клевер'
 # meadow_set =
 # TODO здесь ваш код
 
+garden_set = set(garden)
+meadow_set = set(meadow)
+
 # выведите на консоль все виды цветов
 # TODO здесь ваш код
+
+all_type_flowers = garden_set | meadow_set
+print(all_type_flowers)
 
 # выведите на консоль те, которые растут и там и там
 # TODO здесь ваш код
 
+flower_crossings = garden_set & meadow_set
+print(flower_crossings)
+
+
 # выведите на консоль те, которые растут в саду, но не растут на лугу
 # TODO здесь ваш код
+garden_only = garden_set - meadow_set
+print(garden_only)
 
 # выведите на консоль те, которые растут на лугу, но не растут в саду
 # TODO здесь ваш код
+meadow_only = meadow_set - garden_set
+print(meadow_only)
 
 
